cd22f2919508036f39240f0ea02fb8d8991904b3
Anton Klock <klock.anton@gmail.com>
2023-03-22 16:23:07 +0100

    adding content functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const endPlateVideo = '/endPlate.webm';
+</script>
+
+<div class="container">
+    <video autoplay loop>
+        <source src={endPlateVideo} type="video/webm" />
+        <track kind="captions">
+    </video>
+
+    <div class="content">
+        <h1>Page</h1>
+        <p>Some content</p>
+    </div>
+</div>
+
+<style>
+    .container {
+        position: static;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        margin: 0;
+        padding: 0;
+    }
+
+    .content {
+        color: white;
+    }
+
+    video {
+        position: relative;
+        top: 0%;
+        left: 50%;
+        min-width: 100%;
+        min-height: 100%;
+        width: auto;
+        height: auto;
+        z-index: -100;
+        transform: translateX(-50%) translateY(00%);
+        margin: 0;
+        padding: 0;
+    }
+</style>
