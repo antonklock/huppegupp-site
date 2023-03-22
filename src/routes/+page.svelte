ba032bab6e9d9bc7d5c8f60ce617f01a6a2d19d9
Anton Klock <klock.anton@gmail.com>
2023-03-22 16:37:17 +0100

    adding video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,39 +7,30 @@
         <source src={endPlateVideo} type="video/webm" />
         <track kind="captions">
     </video>
-
-    <div class="content">
-        <h1>Page</h1>
-        <p>Some content</p>
-    </div>
 </div>
 
 <style>
     .container {
-        position: static;
+        position: absolute;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
-        overflow: auto;
+        overflow: hidden;
         margin: 0;
         padding: 0;
     }
 
-    .content {
-        color: white;
-    }
-
     video {
-        position: relative;
-        top: 0%;
+        position: absolute;
+        top: 50%;
         left: 50%;
         min-width: 100%;
         min-height: 100%;
         width: auto;
         height: auto;
         z-index: -100;
-        transform: translateX(-50%) translateY(00%);
+        transform: translateX(-50%) translateY(-50%);
         margin: 0;
         padding: 0;
     }
