4e88a2456981914d7b47b17202f90a756a3d34e1
Anton Klock <klock.anton@gmail.com>
2023-05-17 09:45:44 +0200

    adding coordinate pages

---
+++ src/routes/x4c3kdne/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+	<h1>60.2314 21.5342</h1>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100%;
+		margin: 0;
+		padding: 0;
+	}
+
+	h1 {
+		font-size: 10vw;
+		font-weight: 900;
+		color: #fff;
+		text-shadow: 0 0 10px #000;
+		font-family: sans-serif;
+		color: red;
+	}
+</style>
